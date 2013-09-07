62f0a40e39cae4df3eaffa6c330b05190608f3a4
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-09-07 18:23:05 +0200

    Added Tina her Sol2

--- sol1/README.md
+++ sol1/README.md
@@ -1,3 +1,7 @@
 Solutions to chapter 1
 =====
 
+GS.hs contains examples and functions to be imported
+Sol1.hs contains all Haskell implementations
+
+Exercises.txt contains the written answers of certain questions.
--- sol2/sol2.hs
+++ sol2/sol2.hs
@@ -1,98 +1,29 @@
-module TAMO
+module Sol2 where
 
-where
+import TAMO
 
+-- Exercise 2.15 A propositional contradiction is a formula that yields false for every combination of truth values for its proposition letters. Write Haskell definitions of contradiction tests for propositional functions with one, two and three variables.
 
-{- 2.2
+contradiction1 :: (Bool -> Bool) -> Bool 
+contradiction1 bf = and [not (bf p) | p <- [True, False] ]
 
-P Q  P excl. or Q 
+contradiction2 :: (Bool -> Bool -> Bool) -> Bool
+contradiction2 bf = and [not (bf p q) | p <- [True, False], q <- [True, False]]
 
-t t       f
-t f       t
-f t       t
-f f       f
+contradiction3 :: (Bool -> Bool -> Bool -> Bool) -> Bool 
+contradiction3 bf = and [not (bf p q r) | p <- [True, False], q <- [True, False], r <-[True, False]]
 
--}
-
-{- 2.4
-
-P Q  not (P <=> Q) 
-
-t t      not t = f
-t f      not f = t
-f t      not f = t
-f f      not t = f
-
--}
-
-infix 1 ==>
-infix 1 <=>
-infixr 2 <+>
-
--- Implication
-(==>) :: Bool -> Bool -> Bool
-x ==> y = (not x) || y
-
--- Equivalence
-(<=>) :: Bool -> Bool -> Bool
-x <=> y = x == y
-
--- Exclusive OR
-(<+>) :: Bool -> Bool -> Bool
-x <+> y = x /= y
-
-p = True
-q = False
-formula1 = (not p) && (p ==> q) <=> not (q && (not p))
-
--- 2.5 Example
-valid1 :: (Bool -> Bool) -> Bool
-valid1 bf = (bf True) && (bf False)
-
-valid2 :: (Bool -> Bool -> Bool) -> Bool
-valid2 bf =    (bf True True)
-            && (bf True False)
-            && (bf False True)
-            && (bf False False)
-
-excluded_middle :: Bool -> Bool
-excluded_middle p = p || not p
-
-form1 p q = p ==> (q ==> p)
-form2 p q = (p ==> q) ==> p
+contradict_test1a = contradiction1 (\ p -> not (not p))
+contradict_test2a = contradiction2 (\ p q -> p ==> q)
+contradict_test3a = contradiction3 (\ p q r -> p && (q && r))
 
 
-{- 2.9
+-- Exercise 2.52 Define a function parity :: [Bool] -> Bool that gives True for parity xs just in case an even number of the xss equals True.
 
-P Q  P excl. or Q   (P excl. or Q) exlc. or Q
+parity :: [Bool] -> Bool
+parity xs = length (filter (\b -> b) xs) `rem` 2 == 0
 
-t t       f              t f  = f
-t f       t              f t  = t
-f t       t              t t  = t
-f f       f              f f  = f
+-- Exercise 2.53 Define a function evenNR :: (a -> Bool) -> [a] -> Bool that gives True for evenNR p xs just in case an even number of the xss have property p. (Use the parity function from the previous exercise.)
 
--}
-
-
-logEquiv1 :: (Bool -> Bool) -> (Bool -> Bool) -> Bool
-logEquiv1 bf1 bf2 = (bf1 True <=> bf2 True) && (bf1 False <=> bf2 False)
-
-logEquiv2 :: (Bool -> Bool -> Bool) -> (Bool -> Bool -> Bool) -> Bool
-logEquiv2 bf1 bf2 = and [(bf1 p q) <=> (bf2 p q) | p <- [True,False], q <- [True,False]]
-
-logEquiv3 :: (Bool -> Bool -> Bool -> Bool) -> (Bool -> Bool -> Bool -> Bool) -> Bool
-logEquiv3 bf1 bf2 = and [(bf1 p q r) <=> (bf2 p q r) | p <- [True,False], q <- [True,False], r <- [True,False]]
-
-formula3 p q = p
-formula4 p q = (p <+> q) <+> q
-formula5 p q = p <=> ((p <+> q) <+> q)
-
-
-
--- 2.12
-
-test1 = True == not False || not False == True
-
--- test2 = logEquiv1 id (\ (not p) -> (not p))
-
---test2 = logEquiv1 id (\ p -> False (==>) not p)
+evenNR :: (a -> Bool) -> [a] -> Bool
+evenNR p xs = parity (map p xs)
