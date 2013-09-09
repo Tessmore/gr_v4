b9a97bb188659802c97ddc944ac395dca9d53f5b
nesanica162 <t.curlinovska@gmail.com>
2013-09-09 15:55:35 +0200

    Update Week2.hs

--- week2/Week2.hs
+++ week2/Week2.hs
@@ -18,10 +18,10 @@ triangle :: Integer -> Integer -> Integer -> Shape
 
 triangle x y z 
 			| x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x && x==y && x==z && y==z = Equilateral
-      | x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x &&(x==y && x/=z && z/=y) 
-            || (x/=y && x/=z && z==y) 
-            || (x/=y && z/=y && x==z) = Isosceles 
-		  | x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x && x*x+y*y==z*z = Rectangular
+      			| x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x &&(x==y && x/=z && z/=y) 
+        			 || (x/=y && x/=z && z==y) 
+        			 || (x/=y && z/=y && x==z) = Isosceles 
+		        | x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x && x*x+y*y==z*z = Rectangular
 			| x>0 && y>0 && z>0 && x+y>=z && x+z>=y && y+z>=x && x/=y && x/=z && y/=z = Other
 			| x>0 && y>0 && z>0 && x+y<z || x+z<y || y+z<x = NoTriangle
 			
