61de19ceeb44d0b02098f4edf75a3387d0e60f83
nesanica162 <t.curlinovska@gmail.com>
2013-09-09 15:54:27 +0200

    Update Week2.hs

--- week2/Week2.hs
+++ week2/Week2.hs
@@ -2,7 +2,7 @@
 
 Group GR_V_4: 
 
-  Fabiën Tesselaar, 
+  Fabiï¿½n Tesselaar, 
   Tim Gosen, 
   Lulu Zhang, 
   Tina Churlinovska
@@ -15,3 +15,14 @@ data Shape = NoTriangle | Equilateral
             | Isosceles | Rectangular | Other deriving (Eq,Show)
 
 triangle :: Integer -> Integer -> Integer -> Shape
+
+triangle x y z 
+			| x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x && x==y && x==z && y==z = Equilateral
+      | x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x &&(x==y && x/=z && z/=y) 
+            || (x/=y && x/=z && z==y) 
+            || (x/=y && z/=y && x==z) = Isosceles 
+		  | x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x && x*x+y*y==z*z = Rectangular
+			| x>0 && y>0 && z>0 && x+y>=z && x+z>=y && y+z>=x && x/=y && x/=z && y/=z = Other
+			| x>0 && y>0 && z>0 && x+y<z || x+z<y || y+z<x = NoTriangle
+			
+{- So this is one trivial solution that need to be implemented with predicate logic  suppose :D -}
