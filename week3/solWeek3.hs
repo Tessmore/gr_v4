4bffaf71d74718eec52a0a1d31f0a9f4e5239ff1
zll19910516 <ella516@qq.com>
2013-09-22 17:33:21 +0200

    add Exercise 5

--- week3/solWeek3.hs
+++ week3/solWeek3.hs
@@ -26,20 +26,21 @@ import Control.Monad
 genIntList :: IO [Int]
 genIntList = liftM (randomRs (0, 10000)) newStdGen
 {- Another solution:
-
-  getIntList :: IO [Int]
-  getIntList = do 
-				d <- getRandomInt 8
-				n <- getRandomInt 5
-				getIntL d n
-
-  getIntL :: Int -> Int -> IO [Int]
-  getIntL _ 0 = return []
-  getIntL d n = do 
-			f <- getRandomInt d
-			fs <- getIntL d (n-1)
-			return (f:fs)
+	This solution can generate random integer lists with finite length (0~5)
 -}
+getIntList2 :: IO [Int]
+getIntList2 = do 
+		d <- getRandomInt 8
+		n <- getRandomInt 5
+		getIntL d n
+
+getIntL :: Int -> Int -> IO [Int]
+getIntL _ 0 = return []
+getIntL d n = do 
+	        f <- getRandomInt d
+                fs <- getIntL d (n-1)
+		return (f:fs)
+
 
 
 {- Assignment 4
@@ -76,7 +77,21 @@ isPermutationSlow (h:t) b
   | (elem h) b = isPermutationSlow t (delete h b)
   | otherwise  = False
   
-  
+
+{- Assignment 5
+  Time spent 1 minutes
+
+  + Test whether 2 random integer lists are permutations of each other
+  + Two random integer lists are genetated from getIntList2 method.
+-}
+testPermutation :: IO ()
+testPermutation =  do 
+                   m <- getIntList2
+                   n <- getIntList2
+                   if isPermutation m n
+                        then print (show m ++ " is a permutation of " ++ show n)
+                   else error (show m ++ " is not a permutation of " ++ show n)
+                   
   
 {- Exercise 7 -}
 cnfTest i = testForms i (\form -> equiv form (cnf form))
