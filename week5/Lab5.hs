49dc4ba2a44ab0122641e4791d898d2ad8e81d2a
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-09-30 14:05:03 +0200

    Added ass1, ass2 but not with assertions yet

--- week5/Lab5.hs
+++ week5/Lab5.hs
@@ -15,17 +15,31 @@ mergeSrt :: Ord a => [a] -> [a]
 mergeSrt [] = []
 mergeSrt (x:xs) = merge [x] (mergeSrt xs)
 
+-- Hlint suggestion :
+mergeSort :: Ord a => [a] -> [a]
+mergeSort = foldr (\ x -> merge [x]) []
+
+-- TODO fix a Prop, fix an assert something
+--mergeSortProp :: Ord a => [a] -> [a] -> Bool
+--mergeSortProp xs 
+
+--mergeSrtA :: Ord a => [a] -> [a]
+--mergeSrtA = assert? mergeSortProp $ assert? someOtherProp mergeSrt
 
 {- 
   Assignment 2.
 
   Implementing merge sort by splitting the list to be sorted in equal parts.
-
-  Also find a suitable assertion of this version + implement it
+  Also implement a suitable assertion of this version
 -}
 
 split :: [a] -> ([a],[a])
 split xs = let
-    n = (length xs) `div` 2
+    n = length xs `div` 2
   in
     (take n xs, drop n xs)
+    
+mergeSrtSplit []  = []
+mergeSrtSplit [x] = [x]
+mergeSrtSplit xs  = merge (mergeSrt ys) (mergeSrt zs)
+                      where (ys, zs) = split xs
