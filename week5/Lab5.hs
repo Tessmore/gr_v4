a6d1f90bfe1f3fd5127f50ce41f080442de742bf
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-10-01 11:36:14 +0200

    Added assertive functions for 1 and 2

--- week5/Lab5.hs
+++ week5/Lab5.hs
@@ -7,6 +7,7 @@ import Week5
 
 {- 
   Assignment 1.
+  #Time spent: 3 hours
 
   Find a suitable assertion of mergeSrt, and write an assertive version of this.  
 -}
@@ -15,20 +16,26 @@ mergeSrt :: Ord a => [a] -> [a]
 mergeSrt [] = []
 mergeSrt (x:xs) = merge [x] (mergeSrt xs)
 
--- Hlint suggestion :
-mergeSort :: Ord a => [a] -> [a]
-mergeSort = foldr (\ x -> merge [x]) []
+-- Programs?
+mergeSrtTest1 = mergeSrt "asdfsakdfjsakdfjskdfjsdf" 
+mergeSrtTest2 = mergeSrt [9,4,7,2,9,4,2,4,8,6,3,1,0,7,2,3,3,2] 
 
--- TODO fix a Prop, fix an assert something
---mergeSortProp :: Ord a => [a] -> [a] -> Bool
---mergeSortProp xs 
+{- 
+  Assignment 1: Assertive function
+    We want mergeSrt to create a sorted list as a result. This means the output
+    condition should be a sorted list. mergeSrt has one parameter as input, 
+    one for output. Thus we can use "post1" with "sorted" condition to assert
+    mergeSrt into returning a sorted list (or giving a post1 error condition if
+    inpossible).
+-}
+mergeSrtA :: Ord a => [a] -> [a]
+mergeSrtA = post1 sorted mergeSrt
 
---mergeSrtA :: Ord a => [a] -> [a]
---mergeSrtA = assert? mergeSortProp $ assert? someOtherProp mergeSrt
 
 {- 
   Assignment 2.
-
+  #Time spent: 1 hour
+  
   Implementing merge sort by splitting the list to be sorted in equal parts.
   Also implement a suitable assertion of this version
 -}
@@ -41,5 +48,14 @@ split xs = let
     
 mergeSrtSplit []  = []
 mergeSrtSplit [x] = [x]
-mergeSrtSplit xs  = merge (mergeSrt ys) (mergeSrt zs)
+mergeSrtSplit xs  = merge (mergeSrtSplit ys) (mergeSrtSplit zs)
                       where (ys, zs) = split xs
+                      
+{-
+  Assignment 2: Assertive function
+    Basically, this is exactly the same as Assignment 1. We want a post condition
+    of a sorted list, but this time using mergeSrtSplit
+-}
+
+mergeSrtSplitA :: Ord a => [a] -> [a]
+mergeSrtSplitA = post1 sorted mergeSrtSplit
