8dfa981480e7e62b498edac627d2101f87a42ea6
Fabien Tesselaar <fabientesselaar@gmail.com>
2013-10-06 11:50:00 +0200

    Renamed Week5 to week5NRC

--- week5/Lab5.hs
+++ week5/Lab5.hs
@@ -3,7 +3,8 @@ module Lab5
 where
 
 import Data.List
-import Week5
+import Week4 
+import Week5_NRC
 
 {- 
   Assignment 1.
@@ -59,14 +60,3 @@ mergeSrtSplit xs  = merge (mergeSrtSplit ys) (mergeSrtSplit zs)
 
 mergeSrtSplitA :: Ord a => [a] -> [a]
 mergeSrtSplitA = post1 sorted mergeSrtSplit
-
-exampleLab5 :: Grid
-exampleLab5 = [[0,0,0,3,0,0,0,0,0],
-               [0,0,0,7,0,0,3,0,0],
-               [2,0,0,0,0,0,0,0,8],
-               [0,0,6,0,0,5,0,0,0],
-               [0,9,1,6,0,0,0,0,0],
-               [3,0,0,0,7,1,2,0,0],
-               [0,0,0,0,0,0,0,3,1],
-               [0,8,0,0,4,0,0,0,0],
-               [0,0,2,0,0,0,0,0,0]]
