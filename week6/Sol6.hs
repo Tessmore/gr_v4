d3d7ca44529212a816f36ea0adb9f1d6bc474d76
zll19910516 <ella516@qq.com>
2013-10-07 15:06:13 +0200

    Update Sol6.hs

--- week6/Sol6.hs
+++ week6/Sol6.hs
@@ -7,8 +7,8 @@ import System.Random
 
 exM :: Integer -> Integer -> Integer -> Integer
 exM x y n = if rem y 2 /= 0
-			then (exMod x (y-1) n) * (rem x n)
-			else exMod x y n
+	then (exMod x (y-1) n) * (rem x n)
+	else exMod x y n
 
 exMod :: Integer -> Integer -> Integer -> Integer
 exMod x 1 n = rem x n
