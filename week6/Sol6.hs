9331d2179b2296a4a487f3536917cd985ac01e5f
zll19910516 <ella516@qq.com>
2013-10-07 15:05:46 +0200

    Create Sol6.hs

---
+++ week6/Sol6.hs
@@ -0,0 +1,15 @@
+module Sol6
+
+where
+import Data.List
+import System.Random
+
+
+exM :: Integer -> Integer -> Integer -> Integer
+exM x y n = if rem y 2 /= 0
+			then (exMod x (y-1) n) * (rem x n)
+			else exMod x y n
+
+exMod :: Integer -> Integer -> Integer -> Integer
+exMod x 1 n = rem x n
+exMod x y n = exMod (x^2) (quot y 2) n 
